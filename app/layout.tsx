3387eb60793259c0b73245cff1fd7de95874a31e
RaminMajidi <raminmajididev01@gmail.com>
2024-11-15 20:40:32 +0330

    [change] compeleted chapter16 (Meatdata)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts/Index';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js course dashboard built with App Router. My github account is RaminMajidi',
+  metadataBase: new URL('https://learn-nextjs-15-theta.vercel.app/'),
+};
+
 
 export default function RootLayout({
   children,
